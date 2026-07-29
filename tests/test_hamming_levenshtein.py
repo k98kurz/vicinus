3db85db2c334de20cf4d499ef4de3359405df5bf
Jonathan Voss <k98kurz@gmail.com>
2026-07-28 22:40:47 -0400

    improved Hamming/Levenshtein tests
    - test_hamming_levenshtein.py: added value checks to 4 of the 6 tests

--- tests/test_hamming_levenshtein.py
+++ tests/test_hamming_levenshtein.py
@@ -11,9 +11,13 @@ class TestHammingLev(unittest.TestCase):
     def setUpClass(cls):
         cls.docdb = DocDB()
 
-    def test_hamming_distance_returns_int(self):
+    def test_hamming_distance_returns_int_that_counts_differences(self):
         hd = hamming_distance(self.docdb.get(0).title, self.docdb.get(1).title)
         assert type(hd) is int, type(hd)
+        assert hamming_distance("hello world", "hxllo world") == 1
+        assert hamming_distance("hello world", "hxxlo world") == 2
+        assert hamming_distance("hello world", "hxxlo worl") == 3
+        assert hamming_distance("hello world", "ello world") == 10
 
     def test_hamming_distance_normalized_returns_float(self):
         hd = hamming_distance(
@@ -21,13 +25,18 @@ class TestHammingLev(unittest.TestCase):
         )
         assert type(hd) is float, type(hd)
 
-    def test_hamming_similarity_returns_float(self):
+    def test_hamming_similarity_returns_float_that_is_1_for_same(self):
         hs = hamming_similarity(self.docdb.get(0).title, self.docdb.get(1).title)
         assert type(hs) is float, type(hs)
+        assert hamming_similarity("test", "test") == 1.0
 
-    def test_levenshtein_distance_returns_int(self):
+    def test_levenshtein_distance_returns_int_that_counts_differences(self):
         ld = levenshtein_distance(self.docdb.get(0).title, self.docdb.get(1).title)
         assert type(ld) is int, type(ld)
+        assert levenshtein_distance("hello world", "hxllo world") == 1
+        assert levenshtein_distance("hello world", "hxxlo world") == 2
+        assert levenshtein_distance("hello world", "hxxlo worl") == 3
+        assert levenshtein_distance("hello world", "ello world") == 1
 
     def test_levenshtein_distance_normalized_returns_float(self):
         ld = levenshtein_distance(
@@ -35,11 +44,12 @@ class TestHammingLev(unittest.TestCase):
         )
         assert type(ld) is float, type(ld)
 
-    def test_levenshtein_similarity_returns_float(self):
+    def test_levenshtein_similarity_returns_float_is_1_for_same(self):
         ls = levenshtein_similarity(
             self.docdb.get(0).title, self.docdb.get(1).title
         )
         assert type(ls) is float, type(ls)
+        assert levenshtein_similarity("test", "test") == 1.0
 
 
 if __name__ == '__main__':
